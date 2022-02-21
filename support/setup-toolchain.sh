6cbe6ed1f12c9fde61bd85c9c6c656b24bb6d9ab
Shaun Inman <shaun@panic.com>
2022-02-21 11:38:31 -0500

    updated toolchain blob
    also will use a local miyoomini-toolchain.tar.xz if present in support

--- support/setup-toolchain.sh
+++ support/setup-toolchain.sh
@@ -1,9 +1,17 @@
 #! /bin/sh
 
+TOOLCHAIN_VERSION=v0.0.2
 TOOLCHAIN_TAR="miyoomini-toolchain.tar.xz"
-TOOLCHAIN_URL="https://github.com/shauninman/miyoomini-toolchain-buildroot/releases/download/v0.0.1/$TOOLCHAIN_TAR"
+TOOLCHAIN_URL="https://github.com/shauninman/miyoomini-toolchain-buildroot/releases/download/$TOOLCHAIN_VERSION/$TOOLCHAIN_TAR"
 
-cd /opt
-wget "$TOOLCHAIN_URL"
+if [ -f "./$TOOLCHAIN_TAR" ]; then
+	echo "using local toolchain"
+	cp "./$TOOLCHAIN_TAR" /opt
+	cd /opt
+else
+	echo "getting remote toolchain"
+	cd /opt
+	wget "$TOOLCHAIN_URL"
+fi
 tar xf "./$TOOLCHAIN_TAR"
 rm -rf "./$TOOLCHAIN_TAR"
