672b85fcd5c933f038f8362190ff8cf6a6c28348
Shaun Inman <shaun@panic.com>
2022-02-21 15:02:02 -0500

    minor edits

--- support/setup-toolchain.sh
+++ support/setup-toolchain.sh
@@ -5,13 +5,13 @@ TOOLCHAIN_TAR="miyoomini-toolchain.tar.xz"
 TOOLCHAIN_URL="https://github.com/shauninman/miyoomini-toolchain-buildroot/releases/download/$TOOLCHAIN_VERSION/$TOOLCHAIN_TAR"
 
 if [ -f "./$TOOLCHAIN_TAR" ]; then
-	echo "using local toolchain"
 	cp "./$TOOLCHAIN_TAR" /opt
 	cd /opt
+	echo "extracting local toolchain"
 else
 	cd /opt
 	wget "$TOOLCHAIN_URL"
-	echo "using remote toolchain $TOOLCHAIN_VERSION"
+	echo "extracting remote toolchain $TOOLCHAIN_VERSION"
 fi
 tar xf "./$TOOLCHAIN_TAR"
 rm -rf "./$TOOLCHAIN_TAR"
